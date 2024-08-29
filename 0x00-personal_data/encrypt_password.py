a77c2fa1a9d6e16b0702b5937048858ce7eb9701
Danmwihoti <danielmwihoti@gmail.com>
2024-08-29 17:08:30 +0300

    Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,10 @@ def hash_password(password: str) -> bytes:
     hashed password,which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    is_valid function that expects 2 arguments and returns a boolean.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -4,7 +4,8 @@ Main file
 """
 
 hash_password = __import__('encrypt_password').hash_password
+is_valid = __import__('encrypt_password').is_valid
 
 password = "MyAmazingPassw0rd"
 print(hash_password(password))
-print(hash_password(password))
+print(is_valid(hash_password(password), password))
