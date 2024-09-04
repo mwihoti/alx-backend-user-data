6cb4cd7224e8bbadc2f77c983bc7298cad20da1e
mwihoti <danielmwihoti@gmail.com>
2024-09-04 03:03:22 +0300

    class basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -69,20 +69,16 @@ class BasicAuth:
         Return None if user_email is None or not a string
         Return None if user_pwd is None or not a string
         """
-        if user_email is None or not isinstance(user_email, str):
-            return None
-        if user_pwd is None or not isinstance(user_pwd, str):
-            return None
-
-        users = User.search({"email": user_email})
-        if not users or len(users) == 0:
-            return None
-
-        user = users[0]
-        if not user.is_valid_password(user_pwd):
-            return None
-
-        return user
+        if type(user_email) == str and type(user_pwd) == str:
+            try:
+                users = User.search({'email': user_email})
+            except Exception:
+                return None
+            if len(users) <= 0:
+                return None
+            if users[0].is_valid_password(user_pwd):
+                return users[0]
+        return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
